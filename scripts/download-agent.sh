e50fa01f32c8091f2f90eee89b0bf4fd4b6485bd
rjenkins <ray@streamfold.com>
2025-02-06 14:14:51 -0800

    add download agent

---
+++ scripts/download-agent.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+CWD="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
+
+set -e
+
+ROTEL_ARCH="x86_64-unknown-linux-gnu"
+
+if [ -z "$ROTEL_RELEASE" ]; then
+  echo "Must set ROTEL_RELEASE"
+  exit 1
+fi
+
+ROTEL_FILE="rotel_${ROTEL_RELEASE}_${ROTEL_ARCH}.tar.gz"
+ROTEL_REPO_OWNER=streamfold
+ROTEL_REPO=rotel
+
+$CWD/download-gh-asset.sh $ROTEL_REPO_OWNER $ROTEL_REPO $ROTEL_RELEASE $ROTEL_FILE
+
+tar -O -zxf $ROTEL_FILE rotel > $CWD/../app/rotel/rotel-agent
+rm $ROTEL_FILE
+
+chmod +x $CWD/../app/rotel/rotel-agent
