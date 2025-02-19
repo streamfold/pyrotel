a7bc67a547bdd85700bec47bf978851bff4c36e2
Mike Heffner <mikeh@fesnel.com>
2025-02-19 18:05:22 -0500

    Future annotations

--- tests/utils.py
+++ tests/utils.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import time
 
 def wait_until(max_wait_secs: int, increment: float, test_fn):
--- tests/utils_server.py
+++ tests/utils_server.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import threading
 
 from http.server import HTTPServer, BaseHTTPRequestHandler, ThreadingHTTPServer
