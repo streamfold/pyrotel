137bd5899bd9c4eeb4f7a707af0de1f7613b35ae
Mike Heffner <mikeh@fesnel.com>
2025-03-03 11:26:46 -0500

    Switch ports to endpoints

--- tests/test_config.py
+++ tests/test_config.py
@@ -12,18 +12,18 @@ def test_defaults():
     cfg = Config()
 
     assert cfg.is_active() == False
-    assert cfg.options["otlp_grpc_port"] == 4317
-    assert cfg.options["otlp_http_port"] == 4318
+    assert cfg.options["otlp_grpc_endpoint"] == "localhost:4317"
+    assert cfg.options["otlp_http_endpoint"] == "localhost:4318"
 
     agent = cfg.build_agent_environment()
-    assert agent["ROTEL_OTLP_GRPC_PORT"] == "4317"
-    assert agent["ROTEL_OTLP_HTTP_PORT"] == "4318"
+    assert agent["ROTEL_OTLP_GRPC_ENDPOINT"] == "localhost:4317"
+    assert agent["ROTEL_OTLP_HTTP_ENDPOINT"] == "localhost:4318"
 
     assert agent.get("ROTEL_EXPORTER") is None
 
 def test_config_env_basic():
     os.environ["ROTEL_ENABLED"] = "true"
-    os.environ["ROTEL_OTLP_GRPC_PORT"] = "5317"
+    os.environ["ROTEL_OTLP_GRPC_ENDPOINT"] = "localhost:5317"
     os.environ["ROTEL_OTLP_EXPORTER_ENDPOINT"] = "http://foo.example.com:4317"
     os.environ["ROTEL_OTLP_EXPORTER_CUSTOM_HEADERS"] = "api=1234,team=8793"
     os.environ["ROTEL_OTLP_EXPORTER_TLS_SKIP_VERIFY"] = "True"
@@ -31,13 +31,13 @@ def test_config_env_basic():
     cfg = Config()
 
     assert cfg.is_active() == True
-    assert cfg.options["otlp_grpc_port"] == "5317"
+    assert cfg.options["otlp_grpc_endpoint"] == "localhost:5317"
     assert cfg.options["exporter"]["endpoint"] == "http://foo.example.com:4317"
     assert cfg.options["exporter"]["custom_headers"] == list(["api=1234", "team=8793"])
     assert cfg.options["exporter"]["tls_skip_verify"] == True
 
     agent = cfg.build_agent_environment()
-    assert agent["ROTEL_OTLP_GRPC_PORT"] == "5317"
+    assert agent["ROTEL_OTLP_GRPC_ENDPOINT"] == "localhost:5317"
     assert agent.get("ROTEL_OTLP_EXPORTER") is None
     assert agent["ROTEL_OTLP_EXPORTER_ENDPOINT"] == "http://foo.example.com:4317"
     assert agent["ROTEL_OTLP_EXPORTER_CUSTOM_HEADERS"] == "api=1234,team=8793"
@@ -54,21 +54,21 @@ def test_config_from_file():
     assert isinstance(cl, Rotel)
 
     assert cl.config.is_active() == True
-    assert cl.config.options["otlp_grpc_port"] == 5317
+    assert cl.config.options["otlp_grpc_endpoint"] == "localhost:5317"
 
     exporter = cl.config.options["exporter"]
     assert exporter["endpoint"] == "http://foo2.example.com:4317"
     assert exporter["custom_headers"] == ["api-key=super-secret", "team=dev"]
 
     agent = cl.config.build_agent_environment()
-    assert agent["ROTEL_OTLP_GRPC_PORT"] == "5317"
+    assert agent["ROTEL_OTLP_GRPC_ENDPOINT"] == "localhost:5317"
     assert agent.get("ROTEL_OTLP_EXPORTER") is None
     assert agent["ROTEL_OTLP_EXPORTER_ENDPOINT"] == "http://foo2.example.com:4317"
     assert agent["ROTEL_OTLP_EXPORTER_CUSTOM_HEADERS"] == "api-key=super-secret,team=dev"
 
 def test_config_validation():
     cfg = Config(Options(
-        otlp_grpc_port = 4317
+        otlp_grpc_endpoint = "localhost:4317"
     ))
     assert cfg.is_active() == False
 
