dec1801d321bf28baeef9f17338404ba16898fbf
Mike Heffner <mikeh@fesnel.com>
2025-07-29 16:22:27 -0400

    Remove printf

--- tests/contrib/trace_processor.py
+++ tests/contrib/trace_processor.py
@@ -5,6 +5,5 @@ from rotel_sdk.open_telemetry.trace.v1 import ResourceSpans
 def process_spans(resource_spans: ResourceSpans):
     for scope_spans in resource_spans.scope_spans:
         for span in scope_spans.spans:
-            print("processing a span\n")
             # Add custom attribute to all spans
             span.attributes.append(KeyValue("processed.by", AnyValue("this_is_trace_processor")))
